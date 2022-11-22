fafb6516b15a4b85973e35a38184fbdcf3fe9646
Krzysztof <ki.zwolak@gmail.com>
2022-11-22 06:17:30 +0100

    Generate html files with webpack

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
-import myName from './myName';
-import './style.css';
-import Lpool from './20220112_153210.jpg';
-import Data from './data.xml';
-import Notes from './data.csv';
+import _ from 'lodash';
+import printMe from './print.js';
 
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-    element.textContent = myName("Chris");
-    element.classList.add('hello');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    const lpool = new Image();
-    lpool.src = Lpool;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    element.appendChild(lpool);
-
-    console.log(Data);
-    console.log(Notes); 
+    element.appendChild(btn);
 
     return element;
 }
